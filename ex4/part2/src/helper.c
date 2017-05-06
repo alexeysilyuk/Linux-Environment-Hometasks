f94bd5d22fff5efba464d88777402dc511b1f23f
Wo1v3r <johnleon24@gmail.com>
2017-05-06 18:54:43 +0300

    helper c helper h

---
+++ ex4/part2/src/helper.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <math.h>
+#include "helper.h"
+
+
+void loggingFunction(char mod,float root1,float root2, float im1, float im2){
+	switch(mod){
+	case 'd':
+		#ifdef VERBOSE_LOGGING
+		printf("Calculating Delta of Equation\n");
+		printf("Delta of Equation is:\n");
+		#endif
+		printf("%.3f\n",root1);
+		break;
+
+
+	case 'c':
+		#ifdef VERBOSE_LOGGING
+			printf("Roots are complex number.\n");
+			printf("Calculating complex roots of Equation\n");
+			printf("Roots of quadratic equation are: \n");
+		#endif
+		printf("%.3f%+.3fi \n",root1,im1);
+		printf("%.3f%+.3fi \n",root2,im2);
+		break;
+
+	case '1':
+		#ifdef VERBOSE_LOGGING
+			printf("Both roots are equal.\n");
+			printf("Calculating root of Equation\n");
+	   		printf("Root of quadratic equation is\n");
+		#endif
+		printf("%.3f \n", root1);
+		break;
+
+	case '2':
+		#ifdef VERBOSE_LOGGING
+			printf("Roots are real numbers.\n");
+			printf("Calculating real roots of Equation\n");
+			printf("Roots of quadratic equation are: \n");
+		#endif
+		printf("%.3f \n", root1);
+		printf("%.3f \n", root2);
+		break;
+		
+	default:
+		printf("Error\n");
+		
+	}
+
+}
+
+
+void solveEquation(float a, float b, float c){
+	float d = getDelta(a,b,c);
+	if (d < 0) solveComplex(a,b,c,d);
+	else if (d == 0) solveOneRoot(a,b,c);
+	else solveTwoRoots(a,b,c,d);
+}
+
+
+
+
+
+
+void solveComplex(float a, float b , float c, float d){
+	float root1 =-b/(2*a);
+	float root2 = root1;
+	float im1 = sqrt(-d)/(2*a);
+	float im2 = -im1;
+
+	loggingFunction('c',root1,root2,im1,im2);
+	return;
+}
+
+void solveOneRoot(float a , float b, float c){
+   	float root = -b /(2* a);
+
+	loggingFunction('1',root,root,0,0);
+	return;
+}
+
+void solveTwoRoots(float a, float b, float c, float d){
+	float root1 = ( -b + sqrt(d)) / (2* a);
+	float root2 = ( -b - sqrt(d)) / (2* a);
+	loggingFunction('2',root1,root2,0,0);
+	return;
+}	
---
+++ ex4/part2/src/helper.h
@@ -0,0 +1,5 @@
+void loggingFunction(char , float , float,float,float);
+void solveEquation(float, float, float);
+void solveComplex(float, float, float, float);
+void solveOneRoot(float, float, float);
+void solveTwoRoots(float, float, float, float);
